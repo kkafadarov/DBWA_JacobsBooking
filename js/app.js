bb55ff411bcfbdc7391c82fe3167842bb02b5e47
si3792 <simo.t.iliev@gmail.com>
2015-12-06 08:35:45 +0100

    user level added

--- js/app.js
+++ js/app.js
@@ -37,6 +37,8 @@ var dummiesAdmins = [
 
 app.controller('JBookController', function(){
 
+    this.userLevel = 1;
+
     this.myBookings = dummiesMyBookings;
     this.bookables = dummies;
     this.pendingRequests = dummiesRequests;
